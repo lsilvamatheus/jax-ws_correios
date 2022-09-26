67c56af09b82d25298818d4fc8e64e7cac93b844
lsilvamatheus <lsilva.matheus@gmail.com>
2022-09-26 08:47:22 -0300

    Testando serviço consulta de CEP

--- src/main/java/com/lsilva/matheus/App.java
+++ src/main/java/com/lsilva/matheus/App.java
@@ -1,13 +1,26 @@
 package com.lsilva.matheus;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import com.lsilva.matheus.service.AtendeCliente;
+import com.lsilva.matheus.service.EnderecoERP;
+import jakarta.xml.ws.Service;
+
+import javax.xml.namespace.QName;
+import java.net.URL;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String...args ) throws Exception {
+        URL urlCorreios = new URL("https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl");
+
+        QName qNameCorreios = new QName("http://cliente.bean.master.sigep.bsb.correios.com.br/", "AtendeClienteService");
+
+        Service serviceCorreios = Service.create(urlCorreios, qNameCorreios);
+
+        AtendeCliente correios = serviceCorreios.getPort(AtendeCliente.class);
+
+        EnderecoERP endereco= correios.consultaCEP("87023150");
+        System.out.println("Cidade: ".concat(endereco.getCidade()));
+
+
     }
 }
